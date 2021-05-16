027ba68b634cb2969cfe0bb8ab3a714727041d07
euguroglu <e.uguroglu@yahoo.com>
2021-05-16 23:07:30 +0300

    log import

--- kafkastream.py
+++ kafkastream.py
@@ -2,6 +2,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, expr
 from pyspark.sql.types import StructType, StructField, StringType, LongType, DoubleType, IntegerType, ArrayType
 
+from lib.logger import Log4j
+
 if __name__ == "__main__":
     spark = SparkSession \
         .builder \
